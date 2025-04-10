14b51d435e80caf3aa61506443895434d9177b72
1DamnDaniel3 <swagawpkek@gmail.com>
2025-04-11 00:29:22 +0300

    Update README file

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # marian_back
+
+THE DEVELOPMENT MODE RUN WITH "npm run dev"
+
+IN db/config SHOULD BE FILE database.js wigh next info:
+```
+export default {
+    username: 'postgres',
+    password: 'YOUR_PASSWORD',
+    database: 'YOUR_DB_NAME',
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres',
+    logging: false, 
+};
+```
+IN .env SHOULD BE NEXT CONSTS: 
+```
+JWT_SECRET=your_jwt_secret_key
+JWT_SECRET_REFRESH=your_jwt_refresh_secret_key
+PORT=3001
+```
--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:3000', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true // Если вам нужно передавать куки или авторизационные данные
-  }));
+// app.use(cors({
+//     origin: 'http://localhost:3000', 
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true // Если вам нужно передавать куки или авторизационные данные
+//   }));
 
 app.use(express.json());
 app.use(cookieParser()); 
